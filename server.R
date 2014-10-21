4939df305c158d66535f5a5534276bb9b98c504d
Pep Carrera <pepcarrera@gmail.com>
2014-10-21 18:39:17 -0500

    through working ui and model

--- server.R
+++ server.R
@@ -7,19 +7,25 @@ mtcars$cyl <- factor(mtcars$cyl)
 # Create model predicting mpg based on cylinder, horse power, weight, and transmission type (am)
 modelFit <- lm(mpg ~ cyl + hp + wt + am, data = mtcars)
 
-# New data placeholder
-cyl <- 4
-hp <- 40
-wt <- 2
-am <- 1
-newdata <- data.frame(cyl, hp, wt, am)
-newdata$am <- factor(newdata$am)
-newdata$cyl <- factor(newdata$cyl)
-
 # Predict the mpg
 predict(modelFit,newdata=newdata)
 
 # Define server logic required to plot various variables against mpg
-shinyServer(function(input, output) {
-        
-})
+shinyServer(
+        function(input, output) {
+        dataInput <- reactive({
+                df <- data.frame(cyl=input$cyl, hp=input$hp, wt=input$wt, am=input$am)
+                return(list(df=df))
+                })
+                
+                
+                #Render prediction to the screen
+                output$prediction <- renderPrint({
+                        newdata <- dataInput()$df
+                        newdata$am <- factor(newdata$am)
+                        newdata$cyl <- factor(newdata$cyl)
+                        predict(modelFit,newdata=newdata)
+                        })
+
+                }
+        )
--- ui.R
+++ ui.R
@@ -1,12 +1,16 @@
 library(shiny)
 
-# Define UI for miles per gallon application
 shinyUI(pageWithSidebar(
+        headerPanel("Predict Miles Per Gallon"),
+        sidebarPanel(
+                numericInput('cyl', 'Enter Number of Cylinders', 4, min = 4, max = 20, step = 1),
+                numericInput('hp', 'Enter Horse Power', 1, min = 1, max = 500, step = 1),
+                numericInput('wt', 'Enter Weight (lb/1000)', 1, min = 0.1, max = 10, step = 0.1),
+                numericInput('am', 'Automatic (0) or Manual (1)', 0, min = 0, max = 1, step = 1)
         
-        # Application title
-        headerPanel("Miles Per Gallon"),
-        
-        sidebarPanel(),
-        
-        mainPanel()
+        ),      
+        mainPanel(
+                h3('Results of prediction'),
+                textOutput('prediction')
+        )
 ))
